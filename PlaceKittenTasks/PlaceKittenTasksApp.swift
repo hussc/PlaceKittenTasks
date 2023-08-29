ba77561ca4c5a0c39a86b21e0a270b2882b8400f
Hussein ElRyalat <hussein.alryalat@adres.ae>
2023-08-29 06:45:29 +0300

    Initial Commit

---
+++ PlaceKittenTasks/PlaceKittenTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceKittenTasksApp.swift
+//  PlaceKittenTasks
+//
+//  Created by Hussein AlRyalat on 29/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct PlaceKittenTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
